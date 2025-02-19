1c65d997855e2d782ec2ee0bd215504e702c4457
sangyong216 <hansy5029@gmail.com>
2025-02-19 14:34:17 +0900

    Create weather_script.py
    OpenWeather API를 활용해
    서울 날씨 정보를 get방식으로 요청
    응답 결과 중 
    온도(temp), 습도(humidity), 설명(description)을 
    seoul-weather.csv에 저장

---
+++ weather_script.py
@@ -0,0 +1,34 @@
+import requests
+import csv
+from datetime import datetime
+import os
+
+API_KEY = os.getenv("OPENWEATHER_API_KEY")
+CITY = "Seoul"
+URL = f"http://api.openweathermap.org/data/2.5/weather?q={CITY}&appid={API_KEY}&units=metric"
+
+response = requests.get(URL)
+data = response.json()
+
+temp = data["main"]["temp"]
+humidity = data["main"]["humidity"]
+description = data["weather"][0]["description"]
+timezone = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+print(temp, humidity, description, timezone)
+
+# 위의 4개의 데이터를 가지는 csv 파일 생성
+csv_filename = "seoul_weather.csv"
+header = ["timezone", "temp", "humidity", "description"]
+
+file_exist = os.path.isfile(csv_filename)
+
+# mode = "a" -> if not is file -> create
+# if is file -> write
+with open(csv_filename, "a", newline="") as file:
+    writer = csv.writer(file)
+
+    if not file_exist:
+        writer.writerow(header)
+    writer.writerow([timezone, temp, humidity, description])
+
+    print("csv 저장 완료")
